11c04de4457cce42e7445e82f17e9d49fdec5e4d
ShinyMorningStar <shinymorningstar715@gmail.com>
2019-11-19 20:30:30 +0100

    Test

--- test/tests.ts
+++ test/tests.ts
@@ -1,34 +1,20 @@
 // tslint:disable-next-line no-implicit-dependencies
 import { assert } from "chai";
 
-import { ExampleBuidlerRuntimeEnvironmentField } from "../src/ExampleBuidlerRuntimeEnvironmentField";
-
 import { useEnvironment } from "./helpers";
 
 describe("Integration tests examples", function() {
-  describe("Buidler Runtime Environment extension", function() {
+  describe("Happy case", function() {
+    this.timeout(120_000);
     useEnvironment(__dirname + "/buidler-project");
 
-    it("It should add the example field", function() {
-      assert.instanceOf(
-        this.env.example,
-        ExampleBuidlerRuntimeEnvironmentField
-      );
-    });
-
-    it("The example filed should say hello", function() {
-      assert.equal(this.env.example.sayHello(), "hello");
-    });
-  });
-});
-
-describe("Unit tests examples", function() {
-  describe("ExampleBuidlerRuntimeEnvironmentField", function() {
-    describe("sayHello", function() {
-      it("Should say hello", function() {
-        const field = new ExampleBuidlerRuntimeEnvironmentField();
-        assert.equal(field.sayHello(), "hello");
-      });
+    it("Compiles and generates Typechain artifacts", async function() {
+      try {
+        await this.env.run("typechain");
+        assert.isTrue(true);
+      } catch (error) {
+        assert.fail(error.message);
+      }
     });
   });
 });
